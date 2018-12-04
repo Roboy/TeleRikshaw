92b89672137bf85d13d08d0f26cd66bdb3c26145
rainnymig <lego_cyp@163.com>
2018-12-04 23:28:30 +0100

    add unity project containing the TeleRikshaw scene

---
+++ Assets/Scripts/RoboyCommandPublisher.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RosSharp.RosBridgeClient
+{
+    public class RoboyCommandPublisher : Publisher<Messages.Rikshaw.RoboyCommand>
+    {
+
+        private Messages.Rikshaw.RoboyCommand message;
+
+        private Coroutine cou;
+
+        protected override void Start()
+        {
+            base.Start();
+            initializeMessage();
+            cou = StartCoroutine(publishMes());
+        }
+
+        private void initializeMessage()
+        {
+            message = new Messages.Rikshaw.RoboyCommand
+            {
+                cmd = "test_cmd",
+                content = "test_content"
+            };
+        }
+
+        private IEnumerator publishMes()
+        {
+            while (true)
+            {
+                Publish(message);
+                yield return new WaitForSeconds(1);
+            }
+        }
+    }
+
+}
