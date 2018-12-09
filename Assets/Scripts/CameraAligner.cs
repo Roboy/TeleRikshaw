c58dc29f38936e178ebb1464553568a328b8bb6d
verena-roehrl <verena-roehrl@web.de>
2018-12-09 19:34:14 +0100

    Add skeleton script for camera alignment

---
+++ Assets/Scripts/CameraAligner.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class CameraAligner : MonoBehaviour {
+
+    private float headRotation = 0.0f;
+
+	void Update () {
+        headRotation = InputTracking.GetLocalRotation(XRNode.Head).eulerAngles.y;
+        Debug.Log(headRotation);
+
+        if(headRotation >= 30.0f){
+            // TODO: send command to Arduino
+            // check back with timo, how camera platform is controlled: 
+            // threshold, angle etc ...
+        }
+	}
+}
