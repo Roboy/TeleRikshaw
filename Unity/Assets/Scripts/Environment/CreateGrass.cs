391679a5d54845d2e86dc1b834b94b2825460891
rainnymig <lego_cyp@163.com>
2019-03-02 21:01:05 +0100

    add new model of Rickshaw

---
+++ Unity/Assets/Scripts/Environment/CreateGrass.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateGrass : MonoBehaviour
+{
+    public GameObject GrassPrefab = null;
+    public Transform Ground = null;
+    public float Width = 8;
+    public int GrassQuantity = 100;
+
+    private const float GroundHeight = 0.25f;
+
+    private void Start()
+    {
+        if (GrassPrefab == null || Ground == null)
+        {
+            return;
+        }
+
+        while (GrassQuantity-- > 0)
+        {
+            Instantiate(GrassPrefab,
+                new Vector3(Random.Range(-Width, Width), GroundHeight, Random.Range(-Width, Width)), 
+                Quaternion.Euler(0, Random.Range(0, 360), 0), 
+                Ground);
+        }
+    }
+}
---
+++ Unity/Assets/Scripts/Rickshaw/PropellerRotate.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PropellerRotate : MonoBehaviour
+{
+    public float RotateSpeed = 360;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(0, RotateSpeed*Time.deltaTime, 0));
+    }
+}
