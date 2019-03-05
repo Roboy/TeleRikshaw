56554ecf8f09cb92809ec93f178a7b14f97f89ba
verena-roehrl <verena-roehrl@web.de>
2019-03-05 12:49:45 +0100

    Fix speed publisher msg type

--- RaspberryPi/src/rickshaw_control/src/speed_handler.py
+++ RaspberryPi/src/rickshaw_control/src/speed_handler.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 import rospy
-from std_msgs.msg import String
+from std_msgs.msg import Float32
 from nav_msgs.msg import Odometry
 import math
 
 speed_mps = 0.0
 speed_kmh = 0.0
 
-pub = rospy.Publisher('rickshaw_speed', String, queue_size=10)
+pub = rospy.Publisher('rickshaw_speed', Float32, queue_size=10)
 
 def callback(data):
     speed_mps = data.twist.twist.linear.x
@@ -16,7 +16,6 @@ def callback(data):
     
 def speed_handler():
     rospy.init_node('speed_handler_rickshaw', anonymous=True)
-    # TODO odometry
     rospy.Subscriber("odom", Odometry, callback)
     rospy.spin()
 
